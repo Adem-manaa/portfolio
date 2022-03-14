c082e970c2189e4e3f84560670315a6725e5ca05
adem manaa <ja_manaa@esi.dz>
2022-03-14 19:47:21 +0100

    fix deploy 5

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,16 +3,15 @@ import "./footer.css"
 import {BsLinkedin} from 'react-icons/bs'
 import {BsFacebook} from 'react-icons/bs'
 import {RiInstagramFill} from 'react-icons/ri'
-import Typed from "typed.js";
-import { useEffect, useRef } from "react";
+
 
 const Footer = () => {
 
   return (
     <footer>
-      <a href="#" className='footer_logo' > ADAM </a>
+      <a href="#home" className='footer_logo' > ADAM </a>
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
